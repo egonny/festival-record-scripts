242e807d1d5719c8a561be7c69645f24f4bfcc49
egonny <dev@egon.me>
2019-04-12 18:18:38 +0200

    Make sure everything runs on loop

---
+++ coachella-2019/record-all.sh
@@ -0,0 +1,4 @@
+screen -d -m python3 /home/egon/projects/festival-record-scripts/coachella-2019/record-channel.py 1 --output-dir /media/egon/Elements/coachella
+screen -d -m python3 /home/egon/projects/festival-record-scripts/coachella-2019/record-channel.py 2 --output-dir /media/egon/Elements/coachella
+screen -d -m python3 /home/egon/projects/festival-record-scripts/coachella-2019/record-channel.py 3 --output-dir /media/egon/Elements/coachella
+
--- coachella-2019/record-channel.py
+++ coachella-2019/record-channel.py
@@ -28,6 +28,38 @@ def get_channels(region, phase):
 	
 	return data["channels"]
 
+def record_stream(channel, output_dir):
+	channels = get_channels("world", 1)
+	channel_url = "http://www.youtube.com/watch?v={}".format(channels[channel - 1])
+	print("Downloading from", channel_url)
+
+	ydl = YoutubeDL()
+	ydl.add_default_info_extractors()
+	info = ydl.extract_info(channel_url, download=False)
+	if info["uploader_id"] != "coachella":
+		print("Uploader is not coachella, stopping")
+		exit()
+
+	streamlink = Streamlink()
+	streamlink.set_loglevel("debug")
+	streamlink.set_logoutput(sys.stdout)
+	streamlink.set_option("hls-live-edge", 9999999)
+	streamlink.set_option("hls-segment-attempts", 99)
+	streamlink.set_option("hls-segment-threads", 5)
+	streamlink.set_option("hls-segment-timeout", 9999)
+
+	filename = "channel{}-{}.ts".format(channel, int(time.time()))
+	streams = streamlink.streams(channel_url)
+	stream = streams["best"]
+	print("Found stream {}".format(stream.url))
+	print("Writing stream to {}".format(output_dir + "/" + filename))
+	with open(output_dir + "/" + filename, 'wb') as out_file, stream.open() as in_stream:
+		while True:
+			data = in_stream.read(1024)
+			if not data:
+				return
+			out_file.write(data)
+
 parser = argparse.ArgumentParser(description='Record a Coachella channel')
 parser.add_argument('channel')
 parser.add_argument('-o', '--output-dir', default="./streams")
@@ -43,33 +75,5 @@ if channel < 1 or channel > 3:
 	print("Channel must be one of 1, 2 and 3")
 	exit()
 
-channels = get_channels("world", 2)
-channel_url = "http://www.youtube.com/watch?v={}".format(channels[channel - 1])
-print("Downloading from", channel_url)
-
-ydl = YoutubeDL()
-ydl.add_default_info_extractors()
-info = ydl.extract_info(channel_url, download=False)
-if info["uploader_id"] != "coachella":
-	print("Uploader is not coachella, stopping")
-	exit()
-
-streamlink = Streamlink()
-streamlink.set_loglevel("debug")
-streamlink.set_logoutput(sys.stdout)
-streamlink.set_option("hls-live-edge", 9999999)
-streamlink.set_option("hls-segment-attempts", 99)
-streamlink.set_option("hls-segment-threads", 5)
-streamlink.set_option("hls-segment-timeout", 9999)
-
-filename = "channel{}-{}.ts".format(channel, int(time.time()))
-streams = streamlink.streams(channel_url)
-stream = streams["best"]
-print("Found stream {}".format(stream.url))
-print("Writing stream to {}".format(args.output_dir + "/" + filename))
-with open(args.output_dir + "/" + filename, 'wb') as out_file, stream.open() as in_stream:
-	while True:
-		data = in_stream.read(1024)
-		if not data:
-			exit()
-		out_file.write(data)
+while True:
+	record_stream(channel, args.output_dir)
